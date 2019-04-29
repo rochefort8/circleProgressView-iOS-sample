c88c6caed02a04a5f1eb74205158a5d17a0b7aa3
Yuji Ogihara <yuji.ogihara.85@gmail.com>
2019-04-29 15:31:23 +0900

    Initial Commit

---
+++ circleviewTests/circleviewTests.swift
@@ -0,0 +1,34 @@
+//
+//  circleviewTests.swift
+//  circleviewTests
+//
+//  Created by Yuji Ogihara on 2019/04/29.
+//  Copyright © 2019年 Yuji Ogihara. All rights reserved.
+//
+
+import XCTest
+@testable import circleview
+
+class circleviewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
